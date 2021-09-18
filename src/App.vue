e7bbd624fcd81f2c836a9be5f8303aae5263c52a
Alice Berglund <aliceberglund98@gmail.com>
2021-09-18 17:58:42 +0200

    Added Shopping cart

--- src/App.vue
+++ src/App.vue
@@ -3,7 +3,11 @@
     <p class="title">Ridsports</p>
     <img width="110" height="130" alt="Logo" src="./logo.png">
     <p class="title">Affären</p>
-  </div>
+     <div class="shoppingCart" v-on:click="viewCart()">
+     <img width="90" height="70" alt="Logo" src="https://cdn0.iconfinder.com/data/icons/minimal-set-seven/32/minimal-49-512.png">
+     <p class="number">{{number}}</p>
+     </div>
+  </div> 
 </template>
 
 <style>
@@ -22,6 +26,22 @@
   margin-right: 40px;
   font-family: 'Times New Roman';
   font-size: 70px;
+  color: black;
+}
+.shoppingCart {
+  position: absolute;
+  top: 0;
+  right: 0;
+  margin-top: 2%;
+  margin-right: 4%;
+  cursor: pointer;
+}
+.number {
+  position: absolute;
+  top: 22%;
+  left: 55%;
+  transform: translate(-50%, -50%);
+  font-size: 15px;
   color: black;
 }
 </style>
